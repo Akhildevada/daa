dc18edaff053a9177886dd4ca951abc6bbea6aa6
akhildevada <akhildevada07@gmail.com@>
2022-07-25 10:53:56 -0400

    change

---
+++ duplicate 217.py
@@ -0,0 +1,16 @@
+
+#finding distinct duplicate 
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        return len(set(nums))!=len(nums)
+
+
+#class Solution:
+  #  def containsDuplicate(self, nums: List[int]) -> bool:
+  #      n=nums.sort()
+   #     for i in range(len(nums)-1):
+   #         if nums[i]==nums[i+1]:
+      #          return True
+      #  return False 
+        
+        
